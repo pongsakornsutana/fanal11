76edf909f282960e15a7ad67670c624a5a9fb6b4
pongsakorn <78690934+pongsakornsutana@users.noreply.github.com>
2021-06-13 17:21:25 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace final6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
